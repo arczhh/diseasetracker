5ff1f7fa38b46347b4464d9f91dce9d73482ea54
arczhh <arkzh@outlook.com>
2020-05-29 17:54:47 +0700

    Local Database
    -> Insert userlocation
    -> insert hospital marker

--- app/src/main/java/com/confused/disease_tracker/helper/DatabaseHelper.java
+++ app/src/main/java/com/confused/disease_tracker/helper/DatabaseHelper.java
@@ -6,34 +6,44 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import androidx.annotation.Nullable;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "disease_tracker";
     private static final String TAB1 = "userlocation";
+    private static final String TAB2 = "hospital";
+    private static final String TAB3 = "version";
+    private SQLiteDatabase sqLiteDatabase;
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
-        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        this.sqLiteDatabase = this.getWritableDatabase();
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL("CREATE TABLE "+TAB1+"(" +
+        sqLiteDatabase.execSQL("CREATE TABLE "+TAB1+" (" +
                 "LID INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 "LAT DOUBLE," +
                 "LNG DOUBLE"+
+                ")");
+        sqLiteDatabase.execSQL("CREATE TABLE "+TAB2+"(" +
+                "HOSPITALID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "HOSPITALNAME VARCHAR2(80),"+
+                "LAT DOUBLE," +
+                "LNG DOUBLE,"+
+                "DISEASE VARCHAR2(1000)"+
                 ")");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+TAB1);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+TAB2);
         onCreate(sqLiteDatabase);
     }
 
+    // Tab1
     public boolean insertUserLocation(double lat, double lng){
-        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put("LAT", lat);
         contentValues.put("LNG", lng);
@@ -46,9 +56,35 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     }
 
     public Cursor getUserLocationData(){
-     SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
      Cursor res = sqLiteDatabase.rawQuery("SELECT * FROM "+TAB1,null);
      return res;
     }
+
+    public Cursor getUserLastLocationData(){
+        Cursor res = sqLiteDatabase.rawQuery("SELECT * FROM "+TAB1+" ORDER BY LID DESC LIMIT 1",null);
+        return res;
+    }
+
+
+    // Tab2
+    public boolean insertHospital(String hospitalname, double lat, double lng, String disease){
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("HOSPITALNAME", hospitalname);
+        contentValues.put("LAT", lat);
+        contentValues.put("LNG", lng);
+        contentValues.put("DISEASE", disease);
+        long result = sqLiteDatabase.insert(TAB2, null, contentValues);
+        if(result == -1){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    public Cursor getHospitalData(){
+        Cursor res = sqLiteDatabase.rawQuery("SELECT * FROM "+TAB2,null);
+        return res;
+    }
+
 
 }
