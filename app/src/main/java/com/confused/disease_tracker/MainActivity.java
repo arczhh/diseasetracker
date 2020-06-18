62dc46f840e65c4a18fde94b7ee9579c1bedb295
Redrum113 <60166185+Redrum113@users.noreply.github.com>
2020-06-18 18:59:57 +0700

    CheckBox+Button in First Launch
    -- ทำ CheckBox + Button
    -- เช็คว่า CheckฺBox ถูกเช็คหรือไม่ หากกดปุ่ม button(ยอมรับ)

--- app/src/main/java/com/confused/disease_tracker/MainActivity.java
+++ app/src/main/java/com/confused/disease_tracker/MainActivity.java
@@ -30,6 +30,7 @@ import androidx.fragment.app.Fragment;
 
 import android.util.Log;
 import android.view.MenuItem;
+<<<<<<< Updated upstream
 import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
@@ -37,14 +38,37 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     @RequiresApi(api = Build.VERSION_CODES.O)
+=======
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    CheckBox checkbox1;
+    Button button2;
+
+
+>>>>>>> Stashed changes
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Setting.setWindow(this);
         AndroidThreeTen.init(this);
+<<<<<<< Updated upstream
 
         startService();
+=======
+        LocationService mYourService = new LocationService();
+        Intent mServiceIntent = new Intent(this, mYourService.getClass());
+
+        if (!isMyServiceRunning(mYourService.getClass())) {
+            startService(mServiceIntent);
+        }
+>>>>>>> Stashed changes
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
@@ -54,8 +78,35 @@ public class MainActivity extends AppCompatActivity {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                     new HomeFragment()).commit();
         }
+
+
+
+
+
+        checkbox1 = (CheckBox)findViewById(R.id.checkBox1);
+        button2 = (Button)findViewById(R.id.button2);
+
+        checkbox1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                validation();
+            }
+        });
+
+
+
     }
 
+//เช็คว่า CheckBox ถูกติ๊กรึยัง
+    public void validation(){
+        if(!checkbox1.isChecked()){
+            Toast.makeText(MainActivity.this, "กรุณายอมรับข้อตกลงก่อนการใช้งาน", Toast.LENGTH_LONG).show();
+            return;
+        }
+        Toast.makeText(MainActivity.this, "เสร็จสมบูรณ์", Toast.LENGTH_SHORT ).show();
+    }
+
+<<<<<<< Updated upstream
     private void startService(){
 
         LocationService mLocationService = new LocationService();
@@ -70,6 +121,11 @@ public class MainActivity extends AppCompatActivity {
             startService(mServiceIntent3);
         }
     }
+=======
+
+
+
+>>>>>>> Stashed changes
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
             new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -111,4 +167,11 @@ public class MainActivity extends AppCompatActivity {
         Log.i ("Service status", "Not running "+serviceClass.getName());
         return false;
     }
+<<<<<<< Updated upstream
+=======
+
+
+
+
+>>>>>>> Stashed changes
 }
